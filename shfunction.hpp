e0e7fe1de716ffa5a2d21e86c905adb24cf1ebb7
Han Tran <hantran@hantran.cs.utah.edu>
2019-01-07 15:31:19 -0700

    test for Laplace problem: hex8 element, assembly of K

---
+++ shfunction.hpp
@@ -0,0 +1,12 @@
+//
+// Created by Han Tran on 12/5/18.
+//
+
+#ifndef ADAPTIVEMATRIX_SHFUNCTION_H
+#define ADAPTIVEMATRIX_SHFUNCTION_H
+
+double *gauss(int n);
+double *basis_hex8 (double p[]);
+double *dfbasis_hex8 (double p[]);
+
+#endif //ADAPTIVEMATRIX_SHFUNCTION_H
