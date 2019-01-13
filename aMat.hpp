692814f5386d3bcd1051ebe0de2a9ff48f3bb19a
Han Tran <hantran@hantran.cs.utah.edu>
2019-01-13 15:18:04 -0700

    aMat::init, assemble, matvec are added

--- aMat.hpp
+++ aMat.hpp
@@ -53,15 +53,12 @@ namespace par {
         unsigned int      m_uiNumDOFperNode;
         /**@brief number of globlal nodes */
         unsigned long     m_uiNumNodesGlobal;
-
-
-        /**@brief */
+        /**@brief communicator */
         MPI_Comm          m_comm;
-        /**@brief */
+        /**@brief process ID*/
         unsigned int      m_uiRank;
-        /**@brief */
+        /**@brief number of processes */
         unsigned int      m_uiSize;
-
         /**@brief PETSC matrix */
         Mat                 m_pMat;
         /**@brief map[e][local_node]  = global_node */
@@ -73,9 +70,9 @@ namespace par {
 
     public:
         /**
-         * @brief:
-         * @param[in] n_local:
-         * @param[in] dof: degrees of freedoms
+         * @brief: initialize variables of aMat
+         * @param[in] n_local : number of local nodes
+         * @param[in] dof : degree of freedoms per node
          * @param[in] comm: MPI communicator
          * */
         aMat(const unsigned int n_local, const unsigned int dof, MPI_Comm comm);
@@ -83,32 +80,64 @@ namespace par {
         /**
          * @brief:
          * @param[in]
+         * @param[out]
+         */
+        par::Error init(){
+            MatAssemblyBegin(m_pMat,MAT_FINAL_ASSEMBLY);
+            return Error::SUCCESS; // fixme to have a specific error type
+        }
+
+        /**
+         * @brief:
+         * @param[in]
+         * @param[out]
+         */
+        par::Error assemble(){
+            MatAssemblyEnd(m_pMat,MAT_FINAL_ASSEMBLY);
+            return Error::SUCCESS; // fixme to have a specific error type
+        }
+
+        /**
+         * @brief: assembly global stiffness matrix
+         * @param[in] eid : element ID
+         * @param[in] e_mat : element stiffness matrix
+         * @param[in] twin : if element is twinned or not
+         * @param[in] mode = ADD_VALUES : add to existing values of the matrix
          * */
         par::Error set_element_matrix(unsigned int eid, T* e_mat, bool twin, InsertMode mode=ADD_VALUES);
 
         /**
-         * @brief: changing size of matrix
+         * @brief: changing size of matrix (under construction)
          * @param[in]
          * */
         par::Error twin_element(unsigned int e);
+
         /**
-        * @brief: set block matrix
+        * @brief: set block matrix (under construction)
         * @param[in]
         * */
         par::Error set_block(I index, T* block_matrix, unsigned int sz, unsigned int dof);
 
         /**
-        * @brief: merge twined elements to single
+        * @brief: merge twined elements to single (under construction)
         * @param[in]
         * */
         par::Error merge_elements();
 
         /**
-         * @brief:
-         * @param[in]
+         * @brief: matrix vector multiplication
+         * @param[in] v_primary : input primary nodal displacements
+         * @param[in] w_primary : output primary nodal displacements
+         * @param[in] v_twin : input twin nodal displacements (twin elements recently generated)
+         * @param[in] w_twin : output twin nodal displacements (twin elements recently generated)
          * */
         par::Error matvec(std::vector<T>& v_primary, std::vector<T>& v_twin, std::vector<T>& w_primary, std::vector<T>& w_twin);
 
+        /**
+         * @brief: number of nodes per element
+         * @param[in] element type
+         * @param[out] number of nodes
+         * */
         static unsigned int nodes_per_element(par::ElementType etype) {
             switch (etype) {
                 case par::ElementType::TET: return 4; break;
@@ -118,35 +147,39 @@ namespace par {
                 default:
                     return (unsigned int)Error::UNKNOWN_ELEMENT_TYPE;
             }
-
         }
 
         /**
         * @brief: set mapping from element local node to global node
-        * @param[in] map[eid][local_node] = global_node
+        * @param[in] map[eid][local_node_ID]
         * */
-        void set_map(I** map){
+        par::Error set_map(I** map){
             m_ulpMap = map;
+
+            return Error::SUCCESS; // fixme to have a specific error type
         }
 
         /**
         * @brief: set element types
-        * @param[in] nelem: number of total (local) element
-        * @param[in] etype[eid] = type of element eid
+        * @param[in] nelem: number of total local elements
+        * @param[in] etype[eid]
         * */
-        void set_elem_types(unsigned int nelem, par::ElementType* etype){
+        par::Error set_elem_types(unsigned int nelem, par::ElementType* etype){
             m_uiNumElem = nelem;
             m_pEtypes = etype;
+
+            return Error::SUCCESS; // fixme to have a specific error type
         }
 
         /**
          * @brief: print out data for debugging
          */
-        void print_data();
+        par::Error print_data();
 
     }; // end of class aMat
 
 
+    //******************************************************************************************************************
     template <typename T,typename I>
     aMat<T,I>::aMat(const unsigned int n_local,const unsigned int dof, MPI_Comm comm) {
 
@@ -155,7 +188,6 @@ namespace par {
         isSuperLU_Dist = PETSC_FALSE; // PetscStrcmp(mtype,MATSUPERLU_DIST,&isSuperLU_Dist);
 
         m_comm = comm;
-
 
         MPI_Comm_rank(comm, (int*)&m_uiRank);
         MPI_Comm_size(comm, (int*)&m_uiSize);
@@ -179,7 +211,7 @@ namespace par {
         }
     }
 
-
+    //******************************************************************************************************************
     template <typename T,typename I>
     par::Error aMat<T,I>::set_element_matrix(unsigned int eid, T* e_mat, bool twin, InsertMode mode) {
 
@@ -194,11 +226,9 @@ namespace par {
         std::vector<PetscInt> colIndices(num_nodes*dof);
         PetscInt rowId;
 
-        //MatAssemblyBegin(m_pMat,MAT_FLUSH_ASSEMBLY);
         unsigned int index = 0;
         for (unsigned int r = 0; r < num_nodes*dof; ++r) {
             rowId = m_ulpMap[eid][r/dof];
-            //if(m_uiRank==1)std::cout<<"rowID: "<<rowId<<std::endl;
             for (unsigned int c = 0; c < num_nodes*dof; ++c) {
                 colIndices[c] = m_ulpMap[eid][c/dof];
                 values[c] = e_mat[index];
@@ -210,41 +240,84 @@ namespace par {
             // colIndices.clear();
         } // r
 
-        //MatAssemblyEnd(m_pMat,MAT_FLUSH_ASSEMBLY);
+        return Error::SUCCESS; // fixme to have a specific error type
+    }
 
+    //******************************************************************************************************************
+    template <typename T, typename I>
+    par::Error aMat<T,I>::matvec(std::vector<T>& v_primary, std::vector<T>& v_twin, std::vector<T>& w_primary, std::vector<T>& w_twin){
+
+        Vec v, w;
+        PetscScalar value, *array;
+        PetscInt i, nlocal, istart, iend;
+        PetscViewer viewer;
+
+        // number of (local) dof's
+        nlocal = m_uiNumNodes*m_uiNumDOFperNode;
+
+        // initialize vectors
+        VecCreate(m_comm, &v);
+        VecSetSizes(v, nlocal, PETSC_DECIDE);
+        VecSetFromOptions(v);
+        VecCreate(m_comm, &w);
+        VecSetSizes(w, nlocal, PETSC_DECIDE);
+        VecSetFromOptions(w);
+
+        // set values of input vector v_primary for v
+        VecGetArray(v, &array);
+        for (i = 0; i < nlocal; i++){
+            array[i] = v_primary[i];
+        }
+        //PetscViewerASCIIOpen(m_comm, "v.dat", &viewer);
+        //VecView(v,viewer);
+
+        //MatAssemblyBegin(m_pMat,MAT_FINAL_ASSEMBLY);
+        //MatAssemblyEnd(m_pMat,MAT_FINAL_ASSEMBLY);
+
+        // matrix vector multiplication
+        MatMult(m_pMat, v, w);
+
+        //PetscViewerASCIIOpen(m_comm, "mat.dat", &viewer);
+        //MatView(m_pMat,viewer);
+
+        //PetscViewerASCIIOpen(m_comm, "w.dat", &viewer);
+        //VecView(w,viewer);
+
+        // set values of w for output vector w_primary
+        VecGetOwnershipRange(w, &istart, &iend);
+
+        unsigned int index = 0;
+        for (i = istart; i <iend; i++){
+            VecGetValues(w,1,&i,&value);
+            w_primary[index] = value;
+            index++;
+        }
+
+        PetscViewerDestroy(&viewer);
+        VecDestroy(&v);
+        VecDestroy(&w);
         return Error::SUCCESS;
     }
 
+    //******************************************************************************************************************
     template <typename T, typename I>
-    void aMat<T,I>::print_data() {
-        // temporary printing for check
-        std::cout << "rank= " << m_uiRank << " size= " << m_uiSize << std::endl;
-        std::cout << "number of nodes= " << m_uiNumNodes << std::endl;
-        std::cout << "nelem= " << m_uiNumElem << std::endl;
+    par::Error aMat<T,I>::print_data() {
 
-        for (unsigned n = 0; n < m_uiNumElem; n++){
-            printf("element %d\n",n);
-            printf("nodes= %d %d %d %d %d %d %d %d \n", m_ulpMap[n][0], m_ulpMap[n][1], m_ulpMap[n][2],
-                   m_ulpMap[n][3], m_ulpMap[n][4], m_ulpMap[n][5], m_ulpMap[n][6], m_ulpMap[n][7]);
-        }
-
-        // write to file
+        // write matrix to file
         PetscViewer viewer;
-        //char fileName[256];
-        //sprintf(fileName,"gmat_%d_%d.dat",m_uiRank,m_uiSize);
         PetscPrintf(PETSC_COMM_SELF,"Write matrix into file gmat.dat ...\n");
-        MatAssemblyBegin(m_pMat,MAT_FINAL_ASSEMBLY);
-        MatAssemblyEnd(m_pMat,MAT_FINAL_ASSEMBLY);
-        //PetscViewerASCIIOpen(m_comm,fileName,&viewer);
+        //MatAssemblyBegin(m_pMat,MAT_FINAL_ASSEMBLY);
+        //MatAssemblyEnd(m_pMat,MAT_FINAL_ASSEMBLY);
         PetscViewerASCIIOpen(m_comm, "gmat.dat", &viewer);
         MatView(m_pMat,viewer);
         PetscViewerDestroy(&viewer);
 
-        // write to screen
+        // write matrix to screen
         /*MatAssemblyBegin(m_pMat,MAT_FINAL_ASSEMBLY);
         MatAssemblyEnd(m_pMat,MAT_FINAL_ASSEMBLY);
         MatView(m_pMat,PETSC_VIEWER_STDOUT_(m_comm));*/
 
+        return Error::SUCCESS; // fixme to have a specific error type
     }
 
 }; // end of namespace par
