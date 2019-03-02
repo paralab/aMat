637b7c9e08805413e9b23f7104dfdd742e9c777a
Han Tran <hantran@hantran.cs.utah.edu>
2019-03-02 09:15:57 -0700

    add eigen matrix type

--- include/ke_matrix.hpp
+++ include/ke_matrix.hpp
@@ -2,9 +2,27 @@
 // Created by Han Tran on 12/7/18.
 //
 
+
+
 #ifndef ADAPTIVEMATRIX_KE_MATRIX_H
 #define ADAPTIVEMATRIX_KE_MATRIX_H
 
-double *ke_hex8(double *xe);
+#include "Dense"
+
+/**
+* @brief: element stiffness matrix of hex 8-node element of potential problem
+* @param[in] double xe[8*3] physical coordinates of element
+* @param[out] double ke_hex8[8*8] element stiffness matrix
+* */
+void ke_hex8(double *ke, const double *xe);
+
+
+/**
+* @brief: element stiffness matrix of hex 8-node element of potential problem, using Eigen matrix
+* @param[in] double xe(8,3) physical coordinates of element
+* @param[out] double ke_hex8_eig(8,8) element stiffness matrix
+* */
+Eigen::Matrix<double,8,8> ke_hex8_eig(Eigen::Matrix<double,8,3> xe);
+
 
 #endif //ADAPTIVEMATRIX_KE_MATRIX_H
